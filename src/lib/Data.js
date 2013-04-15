db7c3e247a6bdc8c102ef4600e5c532ce414bf46
Evan Verworn <evan@verworn.net>
2013-04-15 19:23:20 -0400

    Will now load from a file. Data.loadFile([filename]), where filename is optional override.

--- src/lib/Data.js
+++ src/lib/Data.js
@@ -12,19 +12,23 @@ var DATA_DRAW = [ -1, -1, 1 ];
 var DATA_WIN_INDEX  = 0;
 var DATA_LOSS_INDEX = 1;
 var DATA_DRAW_INDEX = 2;
+var DATA_FILE = "../data/connect/connect-4.data";
 
+var fs = require('fs');
 /**
  * Data
  * Represents one piece of data.
  * `row` is one row of the input data file.
 **/
 function Data(row){
-  var input = row.split(",",DATA_NUM_ATTRIBUTES);
-  for(var i < 0; i < input.length; i++) {
-    switch(input[i]) {
-      case 'b': input[i] = 0; break;
-      case 'x': input[i] = 1; break;
-      case 'o': input[i] =-1; break;
+  this.input = row.split(",",DATA_NUM_ATTRIBUTES);
+  for(var i = 0; i < this.input.length; i++) {
+    switch(this.input[i]) {
+      case 'b': this.input[i] = 0; break;
+      case 'x': this.input[i] = 1; break;
+      case 'o': this.input[i] =-1; break;
+      default: this.input.splice(i,1);
+    }
   }
   this.output = row.substr(row.lastIndexOf(",")+1); //extracts win/loss/draw
   this.idx;   //index of the `1` in the output array
@@ -42,8 +46,21 @@ function Data(row){
   }
 } //Data()
 
+
+function Load_File(_filename) {
+  var filename = _filename || DATA_FILE;
+  console.log(filename);
+  var data = fs.readFileSync(filename).toString();
+  var rows = data.split('\n');
+  for(var i = 0; i < rows.length; i++) {
+    rows[i] = new Data(rows[i]);
+  }
+  return rows;
+} //Load_File()
+
 module.exports = {
   Row: Data,
+  loadFile : Load_File,
   DATA_NUM_ATTRIBUTES : DATA_NUM_ATTRIBUTES,
   DATA_NUM_OUTPUTS : DATA_NUM_OUTPUTS,
   DATA_WIN_STR : DATA_WIN_STR,
@@ -56,6 +73,3 @@ module.exports = {
   DATA_LOSS_INDEX : DATA_LOSS_INDEX,
   DATA_DRAW_INDEX : DATA_DRAW_INDEX
 };
-
-
-
