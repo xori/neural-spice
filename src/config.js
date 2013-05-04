f26dc2c1e86b300eb902a0db11f1e55a5cbf6720
Evan Verworn <evan@verworn.net>
2013-05-04 13:55:00 -0400

    Change

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ var globals = {
   crossover  : 0.8,
   tournament_size: 50,
   data_size : 1000, //max 18000 for connect4, 6497 for wines
-  data_type : "wines"
+  data_type : "wines",
   random    : function(){ return(2*Math.random() - 1); },
   tanh      : function(x){ return( (Math.exp(2*x)-1) / (Math.exp(2*x)+1) ); },
   siggy     : function(x){ return( 1 / (Math.exp(-x) + 1) ); }
